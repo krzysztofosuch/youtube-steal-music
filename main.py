eab50c38b1c7d1c6e0f832f5f6b04d0f38c48ce9
Krzysztof Osuch <krzysiekos@gmail.pl>
2019-02-05 11:27:07 +0100

    fix off-by-one, remove hardcoded url

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def recode_file(filename):
         if line:
             (time, tags) = line.split(" ", 1)
             seconds = time_to_sec(time)
-            if n < len(split) and split[n+1]:
+            print(n, split[n])
+            if n+1 < len(split) and split[n+1]:
                 (time2, _) = split[n+1].split(" ", 1);
                 seconds_end = time_to_sec(time2)
             else:
@@ -32,7 +33,10 @@ def recode_file(filename):
                 song = audio[seconds*1000:seconds_end*1000]
             else:
                 song = audio[seconds*1000:]
-            (artist, title) = re.split(':|—|\W-\W', tags, 1)
+            if (reverse_tag):
+                (title, artist) = re.split(':|—|\W-\W', tags, 1)
+            else:
+                (artist, title) = re.split(':|—|\W-\W', tags, 1)
             mp3path = "download/%s-%s.mp3"%(artist.strip(), title.strip())
             song.export(mp3path, format="mp3", bitrate="320k")
             audiofile = eyed3.load(mp3path)
@@ -47,43 +51,10 @@ def time_to_sec(time):
     for place, num in enumerate(reversed(time.split(":"))):
         seconds += (60**place)*int(num)
     return seconds
-url = "https://www.youtube.com/watch?v=u__zkAPMtLc" #input("Youtube URL?")
-split = """
-0:10 Ляпис Трубецкой — Воины Света
-3:53  Агата Кристи — Сказочная тайга
-6:45  Танцы минус — Цветут цветы 
-10:57  Ляпис Трубецкой — Ты Кинула 
-14:56  Пикник — Кукла с человеческим лицом
-18:46  Пикник — Кем Бы Ты Ни Был
-22:41  ДДТ — Это все
-27:31  Агата Кристи — Ковер-вертолет
-30:48  Чиж & Co — Вот Пуля Просвистела 
-35:17 Король и Шут — Танец злобного гения 
-39:40 Ляпис Трубецкой — В Платье Белом
-44:38 КняzZ — Пассажир
-48:46 Король и Шут — Ели мясо мужики
-51:00 Би-2 Feat. Чичерина — Мой Рок-Н-Ролл
-57:45 Океан Ельзи — Там, Де Нас Нема
-1:01:12 Ляпис Трубецкой — Я верю
-1:04:17 СерьГа — Хоровод
-1:07:09 Алиса — Трасса E-95
-1:11:03 Агата Кристи — Опиум Для Никого
-1:15:00 Жуки — Батарейка
-1:18:46 ДДТ — Рожденный В СССР
-1:23:15 Ленинград — Свобода 
-1:26:25 Би-2 — Полковнику никто не пишет
-1:31:18  Ольга Кормухина — Путь
-1:36:16 Океан Ельзи — Без Бою
-1:40:35 Ногу Свело! — Идем На Восток
-1:44:06 Король и Шут — Воспоминания О Былой Любви
-1:48:40 Ночные Снайперы — Ты дарила мне розы
-1:51:45 Ляпис Трубецкой — Шут
-1:55:08 Мумий Тролль — Дельфины
-1:59:48 Би-2 Feat. Диана Арбенина — Тише И Тише
-2:03:15 Ленинград — Супер Гуд
-2:05:22 Lascala — Дурочка С Севера
-""".split("\n")
-#read_lines("How to split them?")
+url = input("Youtube URL?")
+split = read_lines("How to split them?")
+reverse_tag = input("reverse title-artist order? [y/N]").lower() == 'y'
+
 if len(split)<2:
     split = None
 ydl_opts = {
